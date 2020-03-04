1d03fed7a91b297b2a24fea83605cf9411f58eb2
maki <>
2020-03-05 00:06:46 +0900

    改良版を作成中

--- puzzleGame/puzzleGame/main.cpp
+++ puzzleGame/puzzleGame/main.cpp
@@ -1,195 +1,150 @@
 #include <iostream>
-#include<unordered_map>
-#include<string>
-#include<vector>
+#include <fstream>
 #include <algorithm>
+enum Object {
+	OBJ_SPACE,
+	OBJ_WALL,
+	OBJ_BLOCK,
+	OBJ_MAN,
 
-enum num
-{
-	brock = 0,
-	//player = 1,
-	//target = 2,
-	point = 3,
-	space = 4,
+	OBJ_UNKNOWN,
 };
 
-struct Vector2
-{
-	int x;
-	int y;
+//関数プロトタイプ
+void readFile(char** buffer, int* size, const char* filename);
 
-	
-
-	/*
-	int clamp(int min,int max,int target)
-	{
-
+//二次元配列クラス
+//テンプレートになじみはあるだろうか？なければ基礎だけでも勉強しておこう。
+//このクラス宣言の中ではTというクラスがあるかのように扱われ、
+//これを使う時にはTのところにintとかboolとか入れて使う。
+template< class T > class Array2D {
+public:
+	Array2D() : mArray(0) {}
+	~Array2D() {
+		delete[] mArray;
+		mArray = 0;  //ポインタに0を入れるのはクセにしよう。
 	}
-	*/
+	void setSize(int size0, int size1) {
+		mSize0 = size0;
+		mSize1 = size1;
+		mArray = new T[size0 * size1];
+	}
+	T& operator()(int index0, int index1) {
+		return mArray[index1 * mSize0 + index0];
+	}
+	const T& operator()(int index0, int index1) const {
+		return mArray[index1 * mSize0 + index0];
+	}
+private:
+	T* mArray;
+	int mSize0;
+	int mSize1;
 };
 
+//状態クラス
+class State {
+public:
+	State(const char* stageData, int size);
+	void update(char input);
+	void draw() const;
+	bool hasCleared() const;
+private:
+	enum Object {
+		OBJ_SPACE,
+		OBJ_WALL,
+		OBJ_BLOCK,
+		OBJ_MAN,
 
+		OBJ_UNKNOWN,
+	};
+	void setSize(const char* stageData, int size);
 
-const char* _brockLetter = "#";
-const char* _playerLetter = "p";
-const char* _targetLetter = "o";
-const char* _pointLetter = ".";
-const char* _spaceLetter = " ";
-
-const int _stageSizeX = 8;
-const int _stageSizeY = 5;
-
-Vector2 _playerPos{ 5,1 };
-std::vector<Vector2> _targetPosList{
-	{2,2},
-{3,2}
+	int mWidth;
+	int mHeight;
+	Array2D< Object > mObjects;
+	Array2D< bool > mGoalFlags;
 };
 
-const std::unordered_map<int, const char*> _letterConverter{
-		{brock,_brockLetter},
-		//{target,_targetLetter},
-		{point,_pointLetter},
-	{space,_spaceLetter},
-};
+//グローバル変数
+State* gState = 0;
 
-int stage[_stageSizeY][_stageSizeX] = {
-	{ 0,0,0,0,0,0,0,0 },
-	{ 0,4,3,3,4,4,4,0 },
-	{ 0,4,4,4,4,4,4,0 },
-	{ 0,4,4,4,4,4,4,0 },
-	{ 0,0,0,0,0,0,0,0 }
-};
-
-void Init()
-{
-}
-
-void endl()
-{
-	std::cout << std::endl;
-}
-
-void Update()
-{
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 8; j++) {
-			//プレイヤーの座標と一致したら
-			if (_playerPos.x == j && _playerPos.y == i)
-			{
-				std::cout << _playerLetter;
-				continue;
-			}
-			bool breakFlag = false;
-			for (const auto& at : _targetPosList) {
-				if (at.x == j && at.y == i)
-				{
-					std::cout << _targetLetter;
-					breakFlag = true;
-					break;
-				}
-			}
-			if (breakFlag == true)
-			{
-				continue;
-			}
-			std::cout << _letterConverter.at(stage[i][j]);
-		}
-		std::cout << std::endl;
+void readFile(char** buffer, int* size, const char* filename) {
+	std::ifstream in(filename);
+	if (!in) {
+		*buffer = 0;
+		*size = 0;
+	}
+	else {
+		in.seekg(0, std::ifstream::end);
+		*size = static_cast<int>(in.tellg());
+		in.seekg(0, std::ifstream::beg);
+		*buffer = new char[*size];
+		in.read(*buffer, *size);
 	}
 
-	
-
-	//char playerMove;
-
-	std::string playerMove;
-	std::cin >> playerMove;
-
-	
-	//範囲外参照対策
-	//サボってるコード brockの数字(0)を参照して壁に入らない判定を入れてるわけじゃなくて、
-	//壁の分を含めた領域の外に出ないようにしている。
-	//壁の定義が変わればアウトなので、コード的にはよくないから適当コード
-
-	if (!strcmp(playerMove.c_str(), "w") && _playerPos.y - 2 >= 0)
-	{
-		for (auto& at : _targetPosList) {
-			if (_playerPos.x == at.x && _playerPos.y - 1 == at.y && stage[_playerPos.y - 2][_playerPos.x] == space)
-			{
-				at.y--;
-			}
-			else if (_playerPos.x == at.x && _playerPos.y - 1 == at.y && stage[_playerPos.y - 2][_playerPos.x] == point)
-			{
-				at.y--;
-			}
-		}
-
-		
-
-		//二次元配列上のfor文では左上からカウントを始めているため、y座標の正と負の方向が逆になっている。
-		_playerPos.y--;
-	}
-	else if (!strcmp(playerMove.c_str(), "a") && _playerPos.x - 2 >= 0)
-	{
-		for (auto& at : _targetPosList) {
-			if (_playerPos.x - 1 == at.x && _playerPos.y == at.y && stage[_playerPos.y][_playerPos.x - 2] == space)
-			{
-				at.x--;
-			}
-			else if (_playerPos.x - 1 == at.x && _playerPos.y == at.y && stage[_playerPos.y][_playerPos.x - 2] == space)
-			{
-				at.x--;
-			}
-		}
-		_playerPos.x--;
-	}
-	else if (!strcmp(playerMove.c_str(), "s") && _playerPos.y + 2 < _stageSizeY)
-	{
-		for (auto& at : _targetPosList) {
-			if (_playerPos.x == at.x && _playerPos.y + 1 == at.y && stage[_playerPos.y + 2][_playerPos.x] == space)
-			{
-				at.y++;
-			}
-			else if (_playerPos.x == at.x && _playerPos.y + 1 == at.y && stage[_playerPos.y + 2][_playerPos.x] == point)
-			{
-				at.y++;
-			}
-		}
-		//二次元配列上のfor文では左上からカウントを始めているため、y座標の正と負の方向が逆になっている。
-		_playerPos.y++;
-	}
-	else if (!strcmp(playerMove.c_str(), "d") && _playerPos.x + 2 < _stageSizeX)
-	{
-		for (auto& at : _targetPosList) {
-			if (_playerPos.x + 1 == at.x && _playerPos.y == at.y && stage[_playerPos.y][_playerPos.x + 2] == space)
-			{
-				at.x++;
-			}
-			else if (_playerPos.x + 1 == at.x && _playerPos.y == at.y && stage[_playerPos.y][_playerPos.x + 2] == space)
-			{
-				at.x++;
-			}
-		}
-		_playerPos.x++;
-	}
-
-
-	endl();
 	
 }
 
 int main()
 {
-	//const int brock = 0;
-	//const int player = 1;
-	//const int circle = 2;
-	//const int dot = 3;
-
-	Init();
-
-	while (true)
-	{
-		Update();
+	//最初のフレームは初期化。最初の状態を描画して終わり。
+	if (!gState) {
+		const char* filename = "stageData.txt";
+		char* stageData;
+		int fileSize;
+		readFile(&stageData, &fileSize, filename);
+		if (!stageData) {
+			std::cout << "stage file could not be read." << std::endl;
+			return;
+		}
+		gState = new State(stageData, fileSize);
+		//後始末
+		delete[] stageData;
+		stageData = 0;
+		//初回描画
+		gState->draw();
+		return; //そのまま終わる
 	}
-	return 0;
+	bool cleared = false;
+	//メインループ
+	//クリアチェック
+	if (gState->hasCleared()) {
+		cleared = true;
+	}
+	//入力取得
+	std::cout << "a:left s:right w:up z:down. command?" << std::endl; //操作説明
+	char input;
+	std::cin >> input;
+	//更新
+	gState->update(input);
+	//描画
+	gState->draw();
 
+	if (cleared) {
+		//祝いのメッセージ
+		std::cout << "Congratulation! you win." << std::endl;
+		delete gState;
+		gState = 0;
+	}
+}
+
+State::State(const char * stageData, int size)
+{
+}
+
+void State::update(char input)
+{
+}
+
+void State::draw() const
+{
+}
+
+bool State::hasCleared() const
+{
+	return false;
+}
+
+void State::setSize(const char * stageData, int size)
+{
 }
